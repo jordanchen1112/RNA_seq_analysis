5b19bb33e87bf076cebb88abb960d04ec6294fe8
Jordan <jordanchen815165@gmail.com>
2023-09-02 23:41:00 +0800

    handle the col names

--- Preprocessing.py
+++ Preprocessing.py
@@ -4,22 +4,34 @@ import pandas as pd
 import os 
 
 class Preprocessing:
-    def __init__(self, path, file_name):
+    def __init__(self, path = './Raw_data/', file_name = 'TS_Count.xlsx', save_path = './Result_file/'):
         self.path = path
         self.file_name = file_name
-        self.data = pd.read_excel(os.path.join(path, file_name))
-    
-    def average_alleles(self, save = False):
+        self.save_path = save_path
+        self.id = None
+        self.sample_cols = None
+        
+    def read_file(self):
+        self.data = pd.read_excel(os.path.join(self.path, self.file_name))
+        cols = list(self.data.columns)
+        self.id = str(cols[0])
+        self.sample_cols = list(cols[1:])
+       
+    def average_alleles(self):
         # Remove the 'A' & 'B' of the transcription_id
-        self.data['transcript_id'] = self.data['transcript_id'].str[:-4]
-        cols = ['Rap1Del_2', 'Rap1Del_3', 'Rap1Del_4', 'Rap1WT_1', 'Rap1WT_2', 'Rap1WT_3']
-        merged_df = self.data.groupby('transcript_id').agg({**{col: 'mean' for col in cols}}).reset_index()
-        self.data = merged_df
+        self.data[self.id] = self.data[self.id].str[:-4]
+        self.data = self.data.groupby(self.id).agg({**{col: 'mean' for col in self.sample_cols}}).reset_index()
+ 
+    def remove_zero(self):
+        self.data = self.data[(self.data != 0).all(axis=1)]
 
-        if save == True:
-            name = 'Preprocess_' + self.file_name
-            self.data.to_excel(os.path.join(self.path, name), index=False)
+    def save_file(self):
+        save_name = 'Preprocess_' + self.file_name
+        self.data.to_excel(os.path.join(self.save_path, save_name), index=False)
 
 if __name__ == '__main__':
-    Preprocessor = Preprocessing(path = './Rap1/Data/', file_name = 'TS_Count.xlsx')
-    Preprocessor.average_alleles(save =True)
+    Rap1 = Preprocessing(file_name = 'TS_Count.xlsx')
+    Rap1.read_file()
+    Rap1.average_alleles()
+    # Rap1.remove_zero()
+    Rap1.save_file()
